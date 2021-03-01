f22e56c07b6b436b379e86c6208937ccd75130c2
Naautiik <hubert.janczaak@gmail.com>
2021-03-01 12:18:55 +0100

    initial_bot

--- tutorial.py
+++ tutorial.py
@@ -1,2 +1,17 @@
-#oh no, this shit doesn't work
-print("yeah it actually does")
+import discord
+import os
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f"logged as {client}")
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content.startswith("dupa"):
+        await message.channel.send("fuck off I'm sleeping")
+
+client.run('')
